c76f9481b3783c7a7435766608ab06af34b42fe3
KeelanJon <info@keelsdesign.co.uk>
2020-01-05 01:59:09 +0000

    Added majority of mobile Styles and menu functionality

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 
 // Caching the DOM
 
+const burgerButton = document.getElementById("myBurgerIcon");
+const mobileMenu = document.getElementById("myMobileMenu");
+
+burgerButton.addEventListener("click", function(){
+    mobileMenu.classList.toggle("mobile-menu-open");
+});
